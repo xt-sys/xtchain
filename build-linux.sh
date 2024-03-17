fc120f4f33a6b02d39d1525758889e0509c4d708
Rafal Kupiec <belliash@codingworkshop.eu.org>
2024-03-17 12:20:00 +0100

    Update LLVM to 18.1.1

--- build-linux.sh
+++ build-linux.sh
@@ -28,7 +28,7 @@ CMAKEVCS="https://gitlab.kitware.com/cmake/cmake.git"
 
 # LLVM Settings
 LLVMDIR="${SRCDIR}/llvm"
-LLVMTAG="llvmorg-18.1.0"
+LLVMTAG="llvmorg-18.1.1"
 LLVMVCS="https://github.com/llvm/llvm-project.git"
 
 # Make Settings
